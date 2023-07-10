3589673cc978e5e2d95ac3d0a1055f8210f01a20
samToni <tonitegarsahidi@gmail.com>
2023-07-10 22:57:08 +0700

    done role system

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,47 +17,59 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'superadmin@halokes.my.id',
-            'password' => Hash::make('password'),
-            'is_active' => TRUE,
-            'email_verified_at' => Carbon::now(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'roles' => json_encode(['ROLE_USER', 'ROLE_ADMIN', 'ROLE_OPERATOR']),
-        ]);
+        // Insert users
+        $users = [
+            [
+                'name' => 'Super Admin',
+                'email' => 'superadmin@halokes.my.id',
+                'password' => Hash::make('password'),
+                'is_active' => true,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Pak Bon Admin',
+                'email' => 'admin@halokes.my.id',
+                'password' => Hash::make('password'),
+                'is_active' => true,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Si Tegar Supervisor',
+                'email' => 'supervisor@halokes.my.id',
+                'password' => Hash::make('password'),
+                'is_active' => true,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Sam Didi Operator',
+                'email' => 'operator@halokes.my.id',
+                'password' => Hash::make('password'),
+                'is_active' => true,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Sam Toni User',
+                'email' => 'user@halokes.my.id',
+                'password' => Hash::make('password'),
+                'is_active' => true,
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
 
-        DB::table('users')->insert([
-            'name' => 'Sam Toni User',
-            'email' => 'user@halokes.my.id',
-            'password' => Hash::make('password'),
-            'is_active' => TRUE,
-            'email_verified_at' => Carbon::now(),
-            'roles' => json_encode(['ROLE_USER']),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
 
-        DB::table('users')->insert([
-            'name' => 'Sam Didi Operator',
-            'email' => 'operator@halokes.my.id',
-            'password' => Hash::make('password'),
-            'email_verified_at' => Carbon::now(),
-            'roles' => json_encode(['ROLE_OPERATOR']),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        ];
 
-        DB::table('users')->insert([
-            'name' => 'Pak Bon Admin',
-            'email' => 'admin@halokes.my.id',
-            'password' => Hash::make('password'),
-            'is_active' => TRUE,
-            'email_verified_at' => Carbon::now(),
-            'roles' => json_encode(['ROLE_ADMIN']),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        DB::table('users')->insert($users);
+
+
     }
 }
