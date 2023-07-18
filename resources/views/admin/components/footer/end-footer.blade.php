97d23f09ea4daec74e27f992c1ab804f09336bf7
samToni <tonitegarsahidi@gmail.com>
2023-07-18 21:35:26 +0700

    fix loading remote asset into localonly

--- resources/views/admin/components/footer/end-footer.blade.php
+++ resources/views/admin/components/footer/end-footer.blade.php
@@ -19,7 +19,7 @@
 <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
 
 <!-- Place this tag in your head or just before your close body tag. -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
+<script async defer src="{{ asset('assets/js/github-buttons.js') }}"></script>
 
 
 {{-- YIELD SECTION FOOTER CODE IS HERE  --}}
--- resources/views/admin/components/header/header.blade.php
+++ resources/views/admin/components/header/header.blade.php
@@ -37,13 +37,13 @@
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon/favicon.ico')}}" />
 
-    <!-- Fonts -->
+    {{-- <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet"
-    />
+    /> --}}
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="{{asset('assets/vendor/fonts/boxicons.css')}}" />
@@ -51,7 +51,7 @@
     <!-- Core CSS -->
     <link rel="stylesheet" href="{{asset('assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
     <link rel="stylesheet" href="{{asset('assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
