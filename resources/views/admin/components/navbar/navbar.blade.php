1100b9c82f4f170e628ff77433d3082aba9f058c
samToni <tonitegarsahidi@gmail.com>
2023-09-02 21:15:57 +0700

    change nav and add search form in user CRUD

--- resources/views/admin/components/navbar/navbar.blade.php
+++ resources/views/admin/components/navbar/navbar.blade.php
@@ -10,9 +10,7 @@
         <!-- Search -->
         <div class="navbar-nav align-items-center">
             <div class="nav-item d-flex align-items-center">
-                <i class="bx bx-search fs-4 lh-0"></i>
-                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..."
-                    aria-label="Search..." />
+               Halo Sam Laravel
             </div>
         </div>
         <!-- /Search -->
--- resources/views/admin/pages/user/index.blade.php
+++ resources/views/admin/pages/user/index.blade.php
@@ -9,19 +9,26 @@
         <!-- Striped Rows -->
         <div class="card">
             <h3 class="card-header">List of User</h3>
-            <div>
-                <form action="{{ url()->full() }}" method="get">
-                    <label for="per_page">Show:</label>
-                    <select id="per_page" name="per_page" onchange="this.form.submit()">
-                        <option value="25" {{ $perPage == 25 ? 'selected' : '' }}>25</option>
-                        <option value="50" {{ $perPage == 50 ? 'selected' : '' }}>50</option>
-                        <option value="100" {{ $perPage == 100 ? 'selected' : '' }}>100</option>
-                        <option value="250" {{ $perPage == 250 ? 'selected' : '' }}>250</option>
-                        <option value="500" {{ $perPage == 500 ? 'selected' : '' }}>500</option>
-                    </select>
-                    <input type="hidden" name="sort_order" value="{{ request()->input('sort_order') }}" />
-                    <input type="hidden" name="sort_field" value="{{ request()->input('sort_field') }}" />
-                </form>
+            <div class="d-flex justify-content-between">
+                <div class="p-2 bd-highlight">
+                    <form action="{{ url()->full() }}" method="get" class="d-flex align-items-center">
+                        <label for="per_page">Show:&nbsp; </label>
+                        <select class="form-select" id="per_page" name="per_page" onchange="this.form.submit()">
+                            <option value="25" {{ $perPage == 25 ? 'selected' : '' }}>25</option>
+                            <option value="50" {{ $perPage == 50 ? 'selected' : '' }}>50</option>
+                            <option value="100" {{ $perPage == 100 ? 'selected' : '' }}>100</option>
+                            <option value="250" {{ $perPage == 250 ? 'selected' : '' }}>250</option>
+                            <option value="500" {{ $perPage == 500 ? 'selected' : '' }}>500</option>
+                        </select>
+                        <input type="hidden" name="sort_order" value="{{ request()->input('sort_order') }}" />
+                        <input type="hidden" name="sort_field" value="{{ request()->input('sort_field') }}" />
+                    </form>
+                </div>
+                <div class="p-2 d-flex align-items-center">
+                    <i class="bx bx-search fs-4 lh-0"></i>
+                    <input type="text" class="form-control border-1 shadow-none bg-light bg-gradient" placeholder="Search..."
+                        aria-label="Search..." />
+                </div>
             </div>
             <div class="table-responsive text-nowrap">
                 <table class="table table-striped table-hover align-middle">
@@ -52,7 +59,7 @@
                     </thead>
                     <tbody>
                         @php
-                            $startNumber = ($perPage * ($page-1)) + 1;
+                            $startNumber = $perPage * ($page - 1) + 1;
                         @endphp
                         @foreach ($users as $user)
                             <tr>
