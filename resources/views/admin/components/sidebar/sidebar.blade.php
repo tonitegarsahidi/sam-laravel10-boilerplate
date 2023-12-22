39f947bc52ec6cb9b70198fb22bde4a3a857dac9
samToni <tonitegarsahidi@gmail.com>
2023-12-23 06:03:34 +0700

    change menu

--- resources/views/admin/components/sidebar/sidebar.blade.php
+++ resources/views/admin/components/sidebar/sidebar.blade.php
@@ -71,7 +71,7 @@
     <ul class="menu-inner py-1">
       <!-- Dashboard -->
       <li class="menu-item active">
-        <a href="index.html" class="menu-link">
+        <a href="{{route('dashboard')}}" class="menu-link">
           <i class="menu-icon tf-icons bx bx-home-circle"></i>
           <div data-i18n="Analytics">Dashboard</div>
         </a>
@@ -81,7 +81,7 @@
       <li class="menu-item">
         <a href="javascript:void(0);" class="menu-link menu-toggle">
           <i class="menu-icon tf-icons bx bx-layout"></i>
-          <div data-i18n="Layouts">Layouts</div>
+          <div data-i18n="Layouts">User Context Menu</div>
         </a>
 
         <ul class="menu-sub">
