dffddc32a39806815ed275a171e97ab204ea6f8a
toni tegar sahidi <tonitegarsahidi@gmail.com>
2024-09-22 19:39:01 +0700

    fix: email reset password now use queue async

--- app/Notifications/CustomResetPassword.php
+++ app/Notifications/CustomResetPassword.php
@@ -4,17 +4,26 @@ namespace App\Notifications;
 use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Log;
 
-class CustomResetPassword extends ResetPasswordNotification implements ShouldQueue
+class CustomResetPassword extends ResetPasswordNotification
 {
+
+    // public $connection = 'database'; // or 'redis', etc.
+    public $queue = 'emails';        // specific queue name
+    public $delay = 2;              // delay in seconds
+
     public function toMail($notifiable)
     {
+        Log::debug("SENDING RESET PASSWORD using queue");
         $resetUrl = route('password.reset', ['token' => $this->token, 'email' => $notifiable->email]);
 
         return (new MailMessage)
-            ->subject('Your Custom Password Reset Subject Qith Queue Email')
-            ->line('We have received a request to reset your password.')
-            ->action('Reset Password', $resetUrl)
-            ->line('Thank you for using our application!');
+        ->subject('Reset Your Password')
+        ->greeting('Hello!')
+        ->line('We received a request to reset your password.')
+        ->action('Reset Password', $resetUrl)
+        ->line('If you did not request a password reset, no further action is required.')
+        ->salutation('Regards, '. config('app.name'));
     }
 }
