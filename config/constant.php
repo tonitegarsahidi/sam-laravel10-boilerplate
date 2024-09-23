50b78577733910a79fe3949ced766250db2d32d3
toni tegar sahidi <tonitegarsahidi@gmail.com>
2024-09-23 10:33:32 +0700

    feat:activate email verify for user which newly register.

--- config/constant.php
+++ config/constant.php
@@ -5,8 +5,9 @@ return [
     'DEFAULT_PAGINATION_PERPAGE'    => 25,
     'keytoken' => env('KEY_TOKEN'),
 
-    'NEW_USER_STATUS_ACTIVE'    => TRUE,
-    'NEW_USER_DEFAULT_ROLES'    => "ROLE_USER",
+    'NEW_USER_STATUS_ACTIVE'        => TRUE,
+    'NEW_USER_DEFAULT_ROLES'        => "ROLE_USER",
+    'NEW_USER_NEED_VERIFY_EMAIL'    => FALSE,
 
     'CRUD' => [
         'PER_PAGE' => 25,
