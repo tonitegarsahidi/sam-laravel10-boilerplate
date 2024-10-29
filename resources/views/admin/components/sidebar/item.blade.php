64b3ba8714780eb006dea1771aee97e32fd297b0
toni tegar sahidi <tonitegarsahidi@gmail.com>
2024-10-29 13:06:05 +0700

    fix: submenu active visibility if a submenu is opened in current url, the parent will be higlighted, the menu is always expanded, and the submenu item will be highlighted.  Adjust fontweight and also color for the submenu item

--- resources/views/admin/components/sidebar/item.blade.php
+++ resources/views/admin/components/sidebar/item.blade.php
@@ -7,26 +7,39 @@
     'subMenuData' => null,
 ])
 
+@php
+    // Check if any of the subMenuData URLs match the current URL
+    $isExpanded = false;
+    if ($subMenuData) {
+        foreach ($subMenuData as $item) {
+            if (request()->fullUrlIs($item['subMenuUrl'])) {
+                $isExpanded = true;
+                break;
+            }
+        }
+    }
+@endphp
+
 {{-- MenuItemRender --}}
 
-<li class="menu-item {{ request()->fullUrl() == $menuUrl ? ' active' : '' }}">
+<li class="menu-item {{ request()->fullUrl() == $menuUrl ? ' active' : '' }} {{ $isExpanded? ' active open': ''}}">
     @if (is_null($subMenuData))
-        <a href="{{$menuUrl}}" class="menu-link">
-    @else
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
+            <a href="{{ $menuUrl }}" class="menu-link">
+        @else
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
     @endif
-    <i class="menu-icon tf-icons {{$menuIcon}}"></i>
+    <i class="menu-icon tf-icons {{ $menuIcon }}"></i>
     <div data-i18n="{{ $menuText }}">{{ $menuText }}</div>
     </a>
-    <!-- {{url()->current()}} -->
+    <!-- {{ url()->current() }} -->
     @if (!is_null($subMenuData))
-        <ul class="menu-sub">
+        <ul class="menu-sub {{ $isExpanded ? 'open' : '' }}">
             @foreach ($subMenuData as $itemSubMenu)
-            <li class="menu-item">
-                <a href="{{$itemSubMenu['subMenuUrl'] }}" class="menu-link">
-                    <div data-i18n="{{$itemSubMenu['subMenuText']}}">{{$itemSubMenu['subMenuText']}} </div>
-                </a>
-            </li>
+                <li class="menu-item {{ request()->fullUrlIs($itemSubMenu['subMenuUrl']) ? 'active' : '' }}">
+                    <a href="{{ $itemSubMenu['subMenuUrl'] }}" class="menu-link">
+                        <div data-i18n="{{ $itemSubMenu['subMenuText'] }}">{{ $itemSubMenu['subMenuText'] }} </div>
+                    </a>
+                </li>
             @endforeach
         </ul>
     @endif
